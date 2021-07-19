2d10f29924894465cf82e2548743bca0d01e3980
oebitw <engomarewies@gmail.com>
2021-07-19 08:21:15 +0300

    phase1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Topbar from './components/topbar/Topbar'
+
+import Sidebar from './components/sidebar/Sidebar'
+
+import Share from './components/share/Share'
+
+import Post from './components/post/Post'
+
+function App() {
+  return (
+    <div className="App">
+        <Topbar />
+        {/* <Sidebar /> */}
+        {/* <Share /> */}
+        <Post />
+    </div>
+  );
+}
+
+export default App;
